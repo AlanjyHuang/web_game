dfd53d6ebba8bb0a100612c2f55fbeffeb3a9dcd
AlanjyHuang <52521773+AlanjyHuang@users.noreply.github.com>
2023-01-13 21:48:18 +0800

    add color

--- server/server1.py
+++ server/server1.py
@@ -28,6 +28,18 @@ async def brocast(msg):
 			#remove the client when it disconnects
 			print("Client disconnected.  Do cleanup")
 			clients.remove(websock)
+			
+async def brocast(msg):
+	print(msg,' brocasting') #print to console
+
+	#iterate the clients
+	for websock in clients:
+		try:
+			await websock.send(msg) #send message to each client
+		except websockets.exceptions.ConnectionClosed:
+			#remove the client when it disconnects
+			print("Client disconnected.  Do cleanup")
+			clients.remove(websock)
 			#pass
 
 #starts the service and run forever
